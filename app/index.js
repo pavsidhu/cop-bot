1c1d72998d01392630c316e9c489c3dd3c39b341
Pav Sidhu <pavsidhu@revisify.com>
2017-12-08 22:14:59 +0000

    Remove redux and default app

--- app/index.js
+++ app/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
 import './app.global.css';
-
-const store = configureStore();
 
 render(
   <AppContainer>
-    <Root store={store} history={history} />
+    <Root />
   </AppContainer>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 if (module.hot) {
@@ -19,9 +16,9 @@ if (module.hot) {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
     render(
       <AppContainer>
-        <NextRoot store={store} history={history} />
+        <NextRoot />
       </AppContainer>,
-      document.getElementById('root')
+      document.getElementById('root'),
     );
   });
 }
