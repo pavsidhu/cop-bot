a448371ef3713194023e78b2db85c167ba6957fb
Pav Sidhu <pavsidhu@revisify.com>
2017-12-21 02:10:32 +0000

    Add safety blocker

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
+import { remote } from 'electron'
 
 import Routes from './routes'
 import Navigation from './components/Navigation'
@@ -9,13 +10,31 @@ const Container = styled.div`
   margin-top: 120px;
 `
 
-const App = () => (
-  <div>
-    <Navigation title="Cop Bot" />
-    <Container>
-      <Routes />
-    </Container>
-  </div>
-)
+class App extends React.Component {
+  async componentWillMount() {
+    try {
+      const response = await fetch('https://s3-eu-west-1.amazonaws.com/cop-bot/valid.json')
+      const data = await response.json()
+
+      console.log(await data)
+
+      if ((await data.valid) === false) throw new Error()
+    } catch (error) {
+      alert('Cop Bot requires an internet connection, check your WiFi and try again.')
+      window.close()
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation title="Cop Bot" />
+        <Container>
+          <Routes />
+        </Container>
+      </div>
+    )
+  }
+}
 
 export default App
