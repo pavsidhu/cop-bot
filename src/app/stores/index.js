71ddf25f0053b19ecf1d85c9f63ea58551f5fa75
Pav Sidhu <pav@pavsidhu.com>
2018-02-25 12:48:12 +0000

    Add back mobx-persist

--- src/app/stores/index.js
+++ src/app/stores/index.js
@@ -1,11 +1,11 @@
-// import { create } from 'mobx-persist'
+import { create } from 'mobx-persist'
 import accountsStore from './accountsStore'
 import ordersStore from './ordersStore'
 import optionsStore from './optionsStore'
 
-// const hydrate = create({})
+const hydrate = create({})
 
-// hydrate('accounts', accountsStore)
-// hydrate('orders', ordersStore)
+hydrate('accounts', accountsStore)
+hydrate('orders', ordersStore)
 
 export default { accountsStore, ordersStore, optionsStore }
